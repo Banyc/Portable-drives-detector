f461160435472c93dda99eb6488f48072f20520f
IchHabeKeinName <36535895+fsxian@users.noreply.github.com>
2018-03-13 20:51:04 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,69 @@
+﻿'https://zhidao.baidu.com/question/191858141.html
+Public Class Form1
+    Dim Watcher As New Watcher_like
+    Private t As System.Threading.Thread '创建一个多线程，防止函数影响系统处理U盘
+    Private Delegate Sub VoidDelegate() '创建一个 委托 来在多线程 执行 主线程 内容
+    Public Const WM_DEVICECHANGE = &H219
+    Public Const DBT_DEVICEARRIVAL = &H8000
+    Public Const DBT_DEVICEREMOVECOMPLETE = &H8004
+    'Dim DriveLetter As String
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = WM_DEVICECHANGE Then
+            Select Case m.WParam
+                Case DBT_DEVICEARRIVAL
+                    'Dim s() As DriveInfo = DriveInfo.GetDrives'.GetDrives 检索计算机上的所有逻辑驱动器的驱动器名称。
+                    'For Each drive As DriveInfo In s
+                    '    If drive.DriveType = DriveType.Removable Then
+                    '        DriveLetter = drive.Name.ToString()
+                    '        Debug.WriteLine("U盘 :" & DriveLetter & "已插入.")
+                    '    End If
+                    'Next
+                    Watcher.Start()
+                    InitiateThreadRefleshListBox()'RefleshListBox 
+                Case DBT_DEVICEREMOVECOMPLETE
+                    'Dim s() As DriveInfo = DriveInfo.GetDrives
+                    'For Each drive As DriveInfo In s
+                    '    If drive.ToString = DriveLetter Then Exit Sub
+                    'Next
+                    'Debug.WriteLine("U盘:" & DriveLetter & "已卸载！")
+                    Watcher.Eject()
+                    Watcher.Start()
+                    InitiateThreadRefleshListBox() 'BUG 这一步不执行
+            End Select
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Watcher.Start() '为了检测启动软件前 是否已经有已有的驱动器
+        InitiateThreadRefleshListBox()
+    End Sub
+
+    Private Sub RefleshListBox() '在lamda函数里面不能用 ByRef
+        Do '等待Watcher._list生成
+            If Watcher._IsListLoaded Then
+                Me.Invoke(New VoidDelegate(Sub() 'BUG 这个会执行两次
+                                               ListBox1.Items.Clear() '先把之前的内容删掉
+                                               'Stop
+                                               For Each item In Watcher._listDriveNames
+                                                   ListBox1.Items.Add(item)
+                                               Next item
+                                           End Sub))
+                Exit Do
+            End If
+        Loop
+        t.Abort()
+    End Sub
+
+    Private Sub InitiateThreadRefleshListBox()
+        t = New Threading.Thread(Sub() '使用一个多线程，防止函数影响系统处理U盘
+                                     RefleshListBox()
+                                 End Sub)
+        t.Start()
+    End Sub
+
+    Private Sub Form1_Closing(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        Watcher.FormExit()
+    End Sub
+End Class
---
+++ Watcher-like.vb
@@ -0,0 +1,66 @@
+﻿Public Class Watcher_like
+    Private _IsStarted As Boolean '检测是否执行 RefleshList 函数
+    Private Const LogPath As String = "Log.txt" '输出Log的地址
+    Private _t As System.Threading.Thread '线程初始器？
+    Public _listDriveNames As New List(Of String) '放当前所有驱动器的名字 记得加NEW
+    Public _IsListLoaded As Boolean  '判断 list是否已经 加载完毕
+    'Public Delegate Sub VoidDelegate() '创建一个 委托 来在多线程 执行 主线程 内容
+
+    Public Sub Start()
+        _t = New Threading.Thread(AddressOf RefleshList) '以多线程的方式运行 Watcher
+        _IsStarted = True
+        WriteLog("Start thread!") '在Log里面表示进行了 开始 操作
+        _t.Start()
+    End Sub
+    '强制停止
+    Public Sub Abort()
+        If _t.IsAlive Then
+            _IsStarted = False
+            WriteLog("Abort the thread!")
+            _t.Abort()
+        Else
+            WriteLog("Cannot abort! Because the thread has been dead!")
+        End If
+    End Sub
+
+    Public Sub Eject()
+        WriteLog("Drive ejects!")
+    End Sub
+
+    Public Sub FormExit()
+        WriteLog("Form exits!" & vbCrLf)
+    End Sub
+
+    Private Sub RefleshList()
+        _IsListLoaded = False '初始化，表示list还没有被载入
+        If _IsStarted Then
+            Try
+                'RefleshList() '把所有驱动器名添加到list里
+                _listDriveNames.Clear() '把上次的记录清除掉
+                'For Each drive In My.Computer.FileSystem.Drives '获得所有驱动器
+                For Each drive In System.IO.DriveInfo.GetDrives  '获得所有驱动器 跟上面的是一样的
+                    If drive.DriveType = System.IO.DriveType.Removable Then '如果驱动器类型是可移动的（U盘）
+                        _listDriveNames.Add(drive.Name)
+                        WriteLog(drive.Name) '顺便把名字写到Log里
+                    End If
+                Next drive
+                _IsListLoaded = True '表示已经完成 list更新
+            Catch ex As Exception
+                WriteLog(ex.ToString)
+            End Try
+        End If
+        Abort()
+    End Sub
+
+    'Public ReadOnly Property GetList As List(Of String)
+    '    Get
+    '        Return _listDriveNames
+    '    End Get
+    'End Property
+
+    Private Sub WriteLog(ByRef writeInText As String)
+        Dim f1 As New System.IO.StreamWriter(LogPath, True) 'true代表是追加（如果本身就不存在的话，创建新的 LogPath）
+        f1.Write(System.DateTime.Now.ToString & " " & writeInText & vbCrLf)
+        f1.Close()
+    End Sub
+End Class
